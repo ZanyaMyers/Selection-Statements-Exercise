977019d205c184dda39dd16a77ad7fec353087fe
ZanyaMyers <myerszanya@gmail.com>
2023-11-07 12:49:35 -0800

    selection-statements-exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            //var favNumber = r.Next(1, 10);
+            var favNumber = 3; 
+
+            Console.WriteLine("Pick a number between 1 and 10");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is  too low");
+            }
+            else if(userInput > favNumber) 
+            {
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You are correct!");
+            }
+
         }
     }
 }
